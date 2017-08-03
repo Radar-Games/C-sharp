9a4482490b694aca6905255876c181b7393b7736
Radar-Games <154015@154015-SCT11837.gateway>
2017-08-03 21:01:07 +0800

    Prime Number Progress and Heads and Tails

---
+++ HeadsOrTails/HeadsOrTails/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HeadsOrTails
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.ReadKey();
+
+                Random rand = new Random();
+
+                double a = rand.Next(1, 100);
+
+                if (a <= 50)
+                {
+                    Console.WriteLine("Heads");
+                }
+
+                if (a >= 51)
+                {
+                    Console.WriteLine("Tails");
+                }
+            }
+        }
+    }
+}
---
+++ PrimeNumbers/PrimeNumbers/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+
+namespace PrimeNumbers
+{
+	class MainClass
+	{
+		static void Main(string[] args)
+		{
+            MainClass v = new MainClass();
+            v.Calculations(int.Parse(Console.ReadLine()));
+		}
+
+        void Calculations(int z)
+        {
+            int pCount = 0;
+            int desired = z;
+            int i = 2;
+
+            while (desired > pCount)
+            {
+                i++;
+
+                bool isPrime = true;
+
+                for (int a = 2; a <= i / 2; a++)
+                {
+                    if (i % a == 0)
+                    {
+                        isPrime = false;
+                        return;
+                    }
+
+                }
+                if(isPrime)
+                {
+                    Console.WriteLine(i);
+                    pCount += 1;
+                }
+            }
+
+            Console.ReadLine();
+		}
+    }
+}
