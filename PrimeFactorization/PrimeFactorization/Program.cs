5238964a9f18c937f66b355ebf4d95b349efceaf
Radar-Games <154015@154015-SCT11837.local>
2017-08-03 09:27:46 +0800

    Prime Factorisation

---
+++ PrimeFactorization/PrimeFactorization/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PrimeFactorization
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your integer: ");
+                                
+            int a = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= a; i++)
+            {
+                if (a % i == 0)
+                {
+                    Console.WriteLine("{0} is a factor of {1}", i, a);
+                }     
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
