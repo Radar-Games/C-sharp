7430d61fe56960c44b162be28a4d5d991d15df80
Radar-Games <154015@154015-SCT11837.local>
2017-08-05 18:36:14 +0800

    Quadratics Done

---
+++ QuadraticAttempt2/QuadraticAttempt2/Commands.cs
@@ -0,0 +1,167 @@
+﻿using System;
+namespace QuadraticAttempt2
+{
+    public class Commands
+    {
+		public double FindA(double y1_1, double y1_2, double y1_3, double y1_4)
+		{
+			// /// Variables
+
+			// // First Set
+			double y2_1, y2_2, y2_3;
+
+			// // Second Set
+			double y3_1, y3_2;
+
+			// // a
+			double a;
+
+
+			// /// Finding 'a'
+
+			// // Finding First Difference Pattern
+
+			// / First Variable
+			y2_1 = y1_2 - y1_1;
+
+			// / Second Variable
+			y2_2 = y1_3 - y1_2;
+
+			// / Third Variable
+			y2_3 = y1_4 - y1_3;
+
+			// // Finding Second Difference Pattern
+
+			// / First Variable
+			y3_1 = y2_2 - y2_1;
+
+			// / Second Variable
+			y3_2 = y2_3 - y2_2;
+
+
+			// /// Checking That Values Given Have Quadratic Relationship
+
+			// // Check
+			if (y3_1 != y3_2)
+			{
+				Console.WriteLine("The variables you added are not quadratic");
+			}
+
+
+			// /// Calculating Variable 'a'
+
+			// // Calculation
+			a = y3_1 / 2;
+
+
+			// /// Returning Variable 'a'
+
+			// // Returning
+			return a;
+		}
+
+		public double FindFirstFirstDifference(double y1_1, double y1_2)
+		{
+			// /// Variables
+
+			// // First Set
+			double y2_1;
+
+
+			// /// Calculating the First First Difference
+
+			// // Calculation
+			y2_1 = y1_2 - y1_1;
+
+
+			// /// Returning the First First Difference
+
+			// // Return
+			return y2_1;
+		}
+
+		public double FindSecondDifference(double y1_1, double y1_2, double y1_3)
+		{
+			// /// Variables
+
+			// // First Diff
+			double y2_1, y2_2;
+
+			// // Second Diff
+			double y3_1;
+
+
+			// /// Calculating Second Difference Pattern
+
+			// // Finding First Difference Pattern
+			y2_1 = y1_2 - y1_1;
+			y2_2 = y1_3 - y1_2;
+
+			// // Finding Second Difference Pattern
+			y3_1 = y2_2 - y2_1;
+
+
+			// /// Returning The Second Difference Pattern
+
+			// // Return
+			return y3_1;
+		}
+
+        public double FindC (double fDifference, double sDifference, int x1, double y1_1)
+        {
+            // ///Variables
+
+            // // Numbers
+            double num = y1_1, fDiff = fDifference;
+
+
+            // /// Calculations
+
+            // // Finding the y Intercept
+            for (int i = x1; i != 0; i--)
+            {
+                fDiff -= sDifference;
+                num -= fDiff;
+            }
+
+            // /// Returning y Intercept
+
+            // // Return
+            return num;
+        }
+
+        public double FindB (double a, double c, double x1, double y1_1, double x2, double y1_2)
+        {
+            // /// Variables
+
+            // // Predicted
+            double current;
+
+            // // Placeholder
+            double i, b;
+            if (x1 != 0)
+            {
+                current = a * x1 * x1 + c;
+
+                i = y1_2 - current;
+
+                b = i / x1;
+            }
+            else
+            {
+				current = a * x2 * x2 + c;
+
+                i = y1_2- current;
+
+				b = i / x2;
+            }
+
+			return b;
+        }
+
+        public void ThoseFuckingNotifications (double a, double b)
+        {
+            // SMH
+        }
+    }
+}
---
+++ QuadraticAttempt2/QuadraticAttempt2/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace QuadraticAttempt2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // /// Creating References
+
+            // // Reference to MainClass to Invoke Methods
+            Commands com = new Commands();
+
+
+            // /// Intro
+
+            // // Telling User Requirements
+            Console.WriteLine("All 4 x values must be in consecutive order, whole numbers and positive");
+
+
+            // /// Creating Variables
+
+            // // Base Values
+            double y1_1, y1_2, y1_3, y1_4;
+            int x1, x2, x3, x4;
+
+			// // Final Values
+			double a, b, c;
+
+            // // Difference Values
+            double sDifference, fDifference;
+
+            // // Testing
+            /*
+            x1 = 0;
+            x2 = 1;
+            x3 = 2;
+            x4 = 4;
+
+            y1_1 = -2;
+            y1_2 = -2;
+            y1_3 = 0;
+            y1_4 = 4;
+            */
+
+			// /// Get Variables
+
+            // // Getting Values from User
+            Console.WriteLine("X:");
+            x1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Y:");
+            y1_1 = int.Parse(Console.ReadLine());
+
+			// // Getting Second Set
+			Console.WriteLine("X:");
+			x2 = int.Parse(Console.ReadLine());
+			Console.WriteLine("Y:");
+			y1_2 = int.Parse(Console.ReadLine());
+
+			// // Getting Third Set
+			Console.WriteLine("X:");
+            x3 = int.Parse(Console.ReadLine());
+			Console.WriteLine("Y:");
+            y1_3 = int.Parse(Console.ReadLine());
+
+			// // Getting Fourth Set
+			Console.WriteLine("X:");
+            x4 = int.Parse(Console.ReadLine());
+			Console.WriteLine("Y:");
+            y1_4 = int.Parse(Console.ReadLine());
+
+			// / Fucking Notifications
+			com.ThoseFuckingNotifications(x3, x4);
+
+
+            // /// Finding 'a'
+
+            // // Invoking Method and Assigning Variable 'a'
+            a = com.FindA (y1_1, y1_2, y1_3, y1_4);
+
+            // // Displaying Variable 'a'
+            //Console.WriteLine (a);
+
+
+            // /// Getting Difference Values
+
+            // // Invoking Method and Assigning Variable 'a'
+            fDifference = com.FindFirstFirstDifference (y1_1, y1_2);
+
+            // // Invoking Method and Assigning Value of the First First Difference Pattern
+            sDifference = com.FindSecondDifference (y1_1, y1_2, y1_3);
+
+
+            // /// Finding 'c'
+
+            // // Invoking Method and Assigning Variable 'c'
+            c = com.FindC (fDifference, sDifference, x1, y1_1);
+
+            // / Displaying Variable 'c'
+            //Console.WriteLine(c);
+
+
+            // /// Finding 'b'
+
+            // // Invoking Method and Assigning Variable 'b'
+            b = com.FindB(a, c, x1, y1_1, x2, y1_2);
+
+            // / Displaying Variable 'b'
+            //Console.WriteLine(b);
+
+            // /// Displaying Quadratic Equation
+
+            // // Displaying
+            Console.WriteLine("{0}x^2 + {1}x + {2}", a, b, c);
+
+            // / Wait for User
+            Console.ReadKey();
+        }
+    }
+}
